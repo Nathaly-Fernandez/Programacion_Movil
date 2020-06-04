c15719fdbf46f58e3c05a893f2be1067941e2608
odyt2112 <odyt_17@hotmail.com>
2020-06-04 01:16:13 -0500

    Update MainActivity.java

--- Programas Movil/app/src/main/java/ec/edu/espe/calculadora/MainActivity.java
+++ Programas Movil/app/src/main/java/ec/edu/espe/calculadora/MainActivity.java
@@ -10,8 +10,10 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     Button suma,resta,multi,div,mmas,mmenos,m,igual,borrar;
     EditText display;
-    int total=0;
+    float total=0;
     int op=0;
+    int op1=1;
+    float memoria=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,82 +40,148 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         igual.setOnClickListener(this);
         borrar.setOnClickListener(this);
     }
+
+    public float sumar(float a,float b){
+        return a+b;
+    }
+    public float restar(float a,float b){
+        return a-b;
+    }
+    public float multiplicar(float a,float b){
+        return a*b;
+    }
+    public float dividir(float a,float b){
+        return a/b;
+    }
+    public float mMas(float a,float b){
+        return a+b;
+    }
+    public float mMenos(float a,float b){
+        return a-b;
+    }
+    public float m(float a){
+        return a;
+    }
+    public void borrrar(){
+        total=0;
+    }
+
     @Override
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.btnSuma:
                 op=1;
-                total=Integer.parseInt(display.getText().toString())+total;
-                display.setText("");
+                if(total==0 || op1==8) {
+                    total=Float.parseFloat(display.getText().toString());
+                    display.setText("");
+                    op1=0;
+                }else {
+                    total=sumar(Float.parseFloat(display.getText().toString()),total);
+                    display.setText("");
+                }
                 break;
             case R.id.btnResta:
                 op=2;
-                if(total==0) {
-                    total=Integer.parseInt(display.getText().toString());
+                if(total==0 || op1==8) {
+                    total=Float.parseFloat(display.getText().toString());
                     display.setText("");
+                    op1=0;
                 }else {
-                    total=total-Integer.parseInt(display.getText().toString());
+                    total=restar(total,Float.parseFloat(display.getText().toString()));
                     display.setText("");
                 }
                 break;
             case R.id.btnMulti:
                 op=3;
-                if(total==0) {
-                    total=Integer.parseInt(display.getText().toString());
+                if(total==0 || op1==8) {
+                    total=Float.parseFloat(display.getText().toString());
                     display.setText("");
+                    op1=0;
                 }else {
-                    total=total*Integer.parseInt(display.getText().toString());
+                    total=multiplicar(total,Float.parseFloat(display.getText().toString()));
                     display.setText("");
                 }
                 break;
             case R.id.btnDiv:
                 op=4;
-                if(total==0) {
-                    total=Integer.parseInt(display.getText().toString());
+                if(total==0 || op1==8) {
+                    total=Float.parseFloat(display.getText().toString());
                     display.setText("");
+                    op1=0;
                 }else {
-                    total=total/Integer.parseInt(display.getText().toString());
-                    display.setText("");
+                    if(display.getText().toString().equals("0")){
+                        total=0;
+                    }else{
+                        total=dividir(total,Float.parseFloat(display.getText().toString()));
+                        display.setText("");
+                    }
                 }
                 break;
             case R.id.btnMmas:
                 op=5;
+                if(memoria == 0){
+                    memoria=Float.parseFloat(display.getText().toString());
+                }else{
+                    memoria=mMas(memoria,Float.parseFloat(display.getText().toString()));
+                }
                 break;
             case R.id.btnMmenos:
                 op=6;
+                if(memoria == 0){
+                    memoria=Float.parseFloat(display.getText().toString());
+                }else{
+                    memoria=mMenos(memoria,Float.parseFloat(display.getText().toString()));
+                }
                 break;
             case R.id.btnM:
                 op=7;
+                memoria=m(Float.parseFloat(display.getText().toString()));
                 break;
             case R.id.btnIgual:
-                switch (op){
-                    case 1:
-                        total=total+Integer.parseInt(display.getText().toString());
-                        break;
-                    case 2:
-                        total=total-Integer.parseInt(display.getText().toString());
-                        break;
-                    case 3:
-                        total=total*Integer.parseInt(display.getText().toString());
-                        break;
-                    case 4:
-                        total=total/Integer.parseInt(display.getText().toString());
-                        break;
-                    case 5:
-                        total=total+Integer.parseInt(display.getText().toString());
-                        break;
-                    case 6:
-                        total=total-Integer.parseInt(display.getText().toString());
-                        break;
-                    case 7:
-                        total=Integer.parseInt(display.getText().toString());
-                        break;
+                if(display.getText().toString().equals("")){
+
+                }else{
+                    switch (op){
+                        case 1:
+                            total=total+Float.parseFloat(display.getText().toString());
+                            break;
+                        case 2:
+                            total=total-Float.parseFloat(display.getText().toString());
+                            break;
+                        case 3:
+                            total=total*Float.parseFloat(display.getText().toString());
+                            break;
+                        case 4:
+                            if(display.getText().toString().equals("0")) {
+                                total=0;
+                                op=9;
+                            }else {
+                                total=total/Float.parseFloat(display.getText().toString());
+                            }
+                            break;
+                        case 5:
+                            total=total+Float.parseFloat(display.getText().toString());
+                            break;
+                        case 6:
+                            total=total-Float.parseFloat(display.getText().toString());
+                            break;
+                        case 7:
+                            total=Float.parseFloat(display.getText().toString());
+                            break;
+                    }
+                }
+                if(op==9){
+                    display.setText("Math Error");
+                    op=0;
+                }else{
+                    display.setText(total+"");
+                    op1=8;
                 }
 
-
-                display.setText(total+"");
                 break;
             case R.id.btnBorrado:
+                total=0;
+                display.setText("");
                 break;
 
 
